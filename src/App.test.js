a3b534e1afcb598218f5316ec54b8e7345c479b2
Christen Shubaly <christen.shubaly@protonmail.com>
2025-07-29 05:19:22 -0400

    new tests none are resolved

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React from 'react'
 import { render, screen } from '@testing-library/react'
 import Reserve_Form from './Core Comps/Reserve_Form'
 import { initializeTimes, updateTimes } from './App'
+import { fetchAPI } from './Core Comps/BookingApi'
+
+jest.mock('./Core Comps/BookingApi', () => ({
+  fetchAPI: jest.fn(),
+}))
+
+
+const localStorageMock = (() => {
+  let store = {}
+  return {
+    getItem: (key) => store[key] || null,
+    setItem: (key, value) => {
+      store[key] = value.toString()
+    },
+    removeItem: (key) => {
+      delete store[key]
+    },
+    clear: () => {
+      store = {}
+    },
+  }
+})()
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock,
+})
 
 test('Renders the BookingForm heading', () => {
   render(<Reserve_Form />)
@@ -10,18 +36,36 @@ test('Renders the BookingForm heading', () => {
 })
 
 describe('initializeTimes', () => {
-  it('returns the correct initial times', () => {
-    const expectedTimes = ['10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00'];
-    const result = initializeTimes()
+  it('returns the correct initial times', async () => {
+    const expectedTimes = ['10:00', '11:00', '12:00']
+    fetchAPI.mockResolvedValue(expectedTimes)
+    const result = await initializeTimes()
     expect(result).toEqual(expectedTimes)
   })
 })
 
 describe('updateTimes', () => {
-  it('returns the same value that is provided in the state', () => {
-    const state = ['10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00'];
-    const action = '2024-07-29';
-    const result = updateTimes(state, action)
-    expect(result).toEqual(state)
+  it('returns the same value that is provided in the state', async () => {
+    const expectedTimes = ['10:00', '11:00', '12:00']
+    fetchAPI.mockResolvedValue(expectedTimes)
+    const result = await updateTimes(null, '2024-07-29')
+    expect(result).toEqual(expectedTimes)
+  })
+})
+
+describe('localStorage', () => {
+  it('should save to local storage', () => {
+    const key = 'testKey'
+    const value = 'testValue'
+    localStorage.setItem(key, value)
+    expect(localStorage.getItem(key)).toBe(value)
+  })
+
+  it('should read from local storage', () => {
+    const key = 'testKey'
+    const value = 'testValue'
+    localStorage.setItem(key, value)
+    const result = localStorage.getItem(key)
+    expect(result).toBe(value)
   })
 })
